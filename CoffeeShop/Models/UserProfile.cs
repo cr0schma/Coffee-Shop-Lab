1d831455e19f41265d37cfbd7f030ac9c9f2766c
Chris Schmalz <cschmalz03@gmail.com>
2023-04-05 21:00:59 -0400

    link on homepage and data validation

--- CoffeeShop/Models/UserProfile.cs
+++ CoffeeShop/Models/UserProfile.cs
@@ -1,10 +1,14 @@
-﻿namespace CoffeeShop.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoffeeShop.Models
 {
     public class UserProfile
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        [EmailAddress]
         public string Email { get; set; }
+        [DataType(DataType.Password)]
         public string Password { get; set; }
     }
 }
